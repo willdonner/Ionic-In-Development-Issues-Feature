49fe3a916a38589cf94e6dba1c38b2a9d047e044
willdonner <765019300@qq.com>
2019-01-23 16:21:03 +0800

    ion-refresh-native🐭

--- ionic_hot-code-push/src/app/app.module.ts
+++ ionic_hot-code-push/src/app/app.module.ts
@@ -6,7 +6,7 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
-
+import { IonRefreshNativeModule } from 'ion-refresh-native';
 @NgModule({
   declarations: [
     MyApp,
@@ -14,6 +14,7 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
+    IonRefreshNativeModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
